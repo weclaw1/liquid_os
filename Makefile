a9653e6090d61d4317f0eba0928948eedd9b9491
Robert Węcławski <r.weclawski@gmail.com>
2017-10-16 23:04:58 +0200

    Long mode enabled

--- Makefile
+++ Makefile
@@ -10,8 +10,12 @@ build/boot.o: boot.asm
 	mkdir -p build
 	nasm -f elf64 boot.asm -o build/boot.o
 
-build/kernel.bin: build/multiboot_header.o build/boot.o linker.ld
-	ld -n -o build/kernel.bin -T linker.ld build/multiboot_header.o build/boot.o
+build/long_mode_init.o: long_mode_init.asm
+	mkdir -p build
+	nasm -f elf64 long_mode_init.asm -o build/long_mode_init.o
+
+build/kernel.bin: build/multiboot_header.o build/boot.o build/long_mode_init.o linker.ld
+	ld -n -o build/kernel.bin -T linker.ld build/multiboot_header.o build/boot.o build/long_mode_init.o
 
 build/os.iso: build/kernel.bin grub.cfg
 	mkdir -p build/isofiles/boot/grub
---
+++ long_mode_init.asm
@@ -0,0 +1,28 @@
+global long_mode_start
+
+section .text
+bits 64
+long_mode_start:
+    ; load 0 into all data segment registers
+    mov ax, 0
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    ;call clear_screen
+
+    ; print `OKAY` to screen
+    mov rax, 0x2f592f412f4b2f4f
+    mov qword [0xb8000], rax
+    hlt
+
+clear_screen:
+    mov eax, 0xb8000 ;start of vga buffer
+    mov ecx, 2000 ;loop 2000 times - 80 columns x 25 rows
+    .start_loop:
+    mov word [eax], 0x0020 ; 0x0020 - blank space
+    add eax, 2 ;increment vga buffer address, each character takes 2 bytes
+    loop .start_loop
+    ret
