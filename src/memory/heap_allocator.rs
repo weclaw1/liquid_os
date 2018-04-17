07094be01cb4deb820017717ab6bf57b56b58624
Robert Węcławski <r.weclawski@gmail.com>
2018-04-17 22:59:20 +0200

    Some more changes to allocator

--- src/memory/heap_allocator.rs
+++ src/memory/heap_allocator.rs
@@ -1,17 +1,16 @@
-use core::alloc::{GlobalAlloc, Layout};
-use core::alloc::Opaque;
+use core::alloc::{GlobalAlloc, Layout, Opaque};
+use core::ptr::NonNull;
 use spin::Mutex;
-use linked_list_allocator::{Heap, LockedHeap, ALLOCATOR};
+use linked_list_allocator::Heap;
 
 pub const HEAP_START: usize = 0o_000_001_000_000_0000;
 pub const HEAP_SIZE: usize = 80 * 4096; // 320 KiB
 
 
-static HEAP: Mutex<Option<&mut Heap>> = Mutex::new(None);
+static HEAP: Mutex<Option<Heap>> = Mutex::new(None);
 
 pub unsafe fn init(offset: usize, size: usize) {
-    ALLOCATOR.init(offset, size);
-    *HEAP.lock() = Some(&mut ALLOCATOR);
+    *HEAP.lock() = Some(Heap::new(offset, size));
 }
 
 pub struct Allocator;
@@ -19,7 +18,9 @@ pub struct Allocator;
 unsafe impl GlobalAlloc for Allocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut Opaque {
         if let Some(ref mut heap) = *HEAP.lock() {
-            heap.allocate_first_fit(layout)
+            heap.allocate_first_fit(layout).ok().map_or(0 as *mut Opaque, |allocation| {
+                allocation.as_ptr()
+            })
         } else {
             panic!("__rust_allocate: heap not initialized");
         }
@@ -27,7 +28,7 @@ unsafe impl GlobalAlloc for Allocator {
 
     unsafe fn dealloc(&self, ptr: *mut Opaque, layout: Layout) {
         if let Some(ref mut heap) = *HEAP.lock() {
-            heap.deallocate(ptr, layout)
+            heap.deallocate(NonNull::new_unchecked(ptr), layout)
         } else {
             panic!("__rust_deallocate: heap not initialized");
         }
